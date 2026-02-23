08a9aa8edf751db6e1e64836fe85ea02722c17d5
David Driscoll <david.driscoll@gmail.com>
2026-02-22 22:03:15 -0500

    updating glance dashboard with some more media

--- kubernetes/apps/equestria/home/glance/resources/test.sh
+++ kubernetes/apps/equestria/home/glance/resources/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # docker run --rm -v .:/app/config/ glanceapp/glance config:print
-op run --no-masking -- docker run -e ROOT_DOMAIN -e AUTHENTIK_API_TOKEN -e cf_accountId -e cf_username -e cf_credential -e equestria_tunnel_username -e equestria_tunnel_credential -e sgc_tunnel_username -e sgc_tunnel_credential -e prowlarr_apikey -e unifi_api_key -e github_token -e grafana_token -e sonarr_api_key -e tautulli_api_key -e sabnzbd_api_key -e jellyfin_api_key -e jellyfin_user_id -e immich_api_key -p 8080:8080 --rm -v .:/app/config/ glanceapp/glance
+op run --no-masking -- docker run -e ROOT_DOMAIN -e AUTHENTIK_API_TOKEN -e CF_ACCOUNTID -e CF_USERNAME -e CF_CREDENTIAL -e EQUESTRIA_TUNNEL_USERNAME -e EQUESTRIA_TUNNEL_CREDENTIAL -e SGC_TUNNEL_USERNAME -e SGC_TUNNEL_CREDENTIAL -e PROWLARR_APIKEY -e UNIFI_API_KEY -e GITHUB_TOKEN -e GRAFANA_TOKEN -e SONARR_API_KEY -e TAUTULLI_API_KEY -e SABNZBD_API_KEY -e JELLYFIN_API_KEY -e PLEX_API_KEY -e JELLYFIN_USER_ID -e IMMICH_API_KEY -p 8080:8080 --rm -v .:/app/config/ glanceapp/glance
 
